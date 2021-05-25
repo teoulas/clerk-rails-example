9563636875a532532acc33cb53540321874405c1
Theodoros Orfanidis <teoulas@gmail.com>
2021-05-25 15:01:03 +0200

    Clerk integration with controller concern

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,7 @@
+class AdminsController < ApplicationController
+  before_action :require_clerk_session!
+
+  def show
+
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
+require "clerk/authenticatable"
+
 class ApplicationController < ActionController::Base
-  def clerk_user
-    request.env["clerk_user"]
-  end
-
-  def clerk_user_signed_in?
-    clerk_user.present?
-  end
-
-  helper_method :clerk_user, :clerk_user_signed_in?
+  include Clerk::Authenticatable
 end
